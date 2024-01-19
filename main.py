2627d556165129784dc59508ee3836810fa46b12
prabhsuratsingh <prabhsurat17@gmail.com>
2024-01-19 13:08:17 +0530

    Get and check bet logic implemented
    Added function get_bet() and checked for enough balance to place bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 MAX_LINES = 3
-
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -29,10 +30,33 @@ def get_number_of_lines():
 
     return lines
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+
+    return amount
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is ${balance}")
+        else:
+            break
+    
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to : ${total_bet}")
 
 main()
