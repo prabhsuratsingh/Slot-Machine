293c2072d50a10a0e449632bb8a03e952d67341c
prabhsuratsingh <prabhsurat17@gmail.com>
2024-01-20 12:40:10 +0530

    Project complete
    Implemented func spin() to allow player to bet until he runs out of balance.

--- main.py
+++ main.py
@@ -108,8 +108,7 @@ def get_bet():
 
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
 
     while True:
@@ -128,5 +127,20 @@ def main():
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
     print(f"You won on lines:", *winning_lines)
+
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is: ${balance}")
+        answer = input("Press enter to spin (q to Quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
+    
 
 main()
